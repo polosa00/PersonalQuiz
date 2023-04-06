e6a86b2b487c6c0a16d1cc5c54d364cd868b3fc7
polosa00 <i@apolochanin.ru>
2023-04-06 23:14:11 +0300

    create logic for app

---
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Александр Полочанин on 3.04.23.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    // MARK: - Private properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    // MARK: - IBActions
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+}
+// MARK: - Private Methods
+private extension QuestionViewController {
+    func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        let currentQuestion = questions[questionIndex]
+        
+        questionLabel.text = currentQuestion.title
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    /// Show stack view with single answers
+    ///
+    /// Display a stack view of single answers category
+    ///
+    /// - Parameter type: Specifies category of responses
+    func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangeStackView(with: currentAnswers)
+        }
+    }
+    
+    func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer)  in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    func showRangeStackView(with answers: [Answer]) {
+        rangedStackView.isHidden.toggle()
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+    
+}
+
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Александр Полочанин on 6.04.23.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
